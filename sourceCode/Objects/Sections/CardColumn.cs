5e003d2a200aedf2738328b5cd92389f2adf26dd
Kamil Kijak <kamil.kijak@pixlesofte.com>
2025-05-11 21:47:24 +0200

    wrong merge with git, delete wrong code

--- sourceCode/Objects/Sections/CardColumn.cs
+++ sourceCode/Objects/Sections/CardColumn.cs
@@ -18,16 +18,6 @@ public class CardColumn : StateObject,IPanel, ICardContainer {
         ActualState = _cards;
     }
 
-    public override void LoadState()
-    {
-        base.LoadState();
-        for (int i = 0; i < _cards.Count - 1; i++) {
-            if(_cards[i].Showed) {
-                _cards[i].Showed = false;
-            }
-        }
-    }
-
     public void ActionPerformed()
     {
         if(_cards.Count != 0) {
